418fa928dcfdc5e4b74763e5938f9e9205a67af2
arecki3000 <ark.kwi@gmail.com>
2020-09-24 23:03:44 +0200

    added footer

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,7 @@ import React from "react"
 import { Container } from "../components"
 
 const IndexPage = () => {
-  return (
-    <Container>
-      <h1>Hello Asia!</h1>
-    </Container>
-  )
+  return <Container></Container>
 }
 
 export default IndexPage
